c06c98dddc13d8c7170f7303d64214a3ce810dde
김현기 <grissom@woowahan.com>
2021-12-24 21:03:05 +0900

    세팅중

---
+++ domain-service/src/main/java/com/tistory/eclipse4j/domain/DomainApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.eclipse4j.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class DomainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DomainApplication.class, args);
+    }
+}
---
+++ domain-service/src/main/java/com/tistory/eclipse4j/domain/jpa/JPATipDb1QuerydslRepositorySupport.java
@@ -0,0 +1,7 @@
+package com.tistory.eclipse4j.domain.jpa;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public abstract class JPATipDb1QuerydslRepositorySupport {
+}
